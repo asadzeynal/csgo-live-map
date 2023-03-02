4ceac3c7f8ef63f0202486ac8b594325135dec2a
asadzeynal <asadzeynal9@gmail.com>
2023-03-02 18:12:19 +0400

    Initial static files

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+var canvasElement;
+var ctx;
+
+function draw() {
+    canvasElement = document.getElementById("game_map");
+    canvasElement.width = window.innerHeight
+    canvasElement.height = window.innerHeight
+
+    ctx = canvasElement.getContext("2d");
+
+    const img = new Image();
+    img.onload = () => {
+        // context.canvas.style.imageRendering = 'auto';
+        ctx.drawImage(img, 0, 0);
+    };
+    img.src = "../img/de_ancient_radar.png";
+};
