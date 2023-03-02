236a8c894d3b01eaf53e449ad64c33abbbfffeb8
asadzeynal <asadzeynal9@gmail.com>
2023-03-02 15:56:49 +0400

    Engine concept change, it is responsible for processing one tick data

--- engine/engine.go
+++ engine/engine.go
@@ -2,17 +2,35 @@ package engine
 
 import "github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/common"
 
-type CurrentGameState struct {
+type IncomingState struct {
 	Players []*common.Player
 }
 
-func processOneTick(state CurrentGameState) {
-
+type StateResult struct {
+	Players []PlayerData
 }
 
-func Run(update chan CurrentGameState) {
-	for {
-		currentState := <-update
-		processOneTick(currentState)
+type PlayerData struct {
+	Position Position
+}
+
+type Position struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+func processOneTick(data IncomingState) StateResult {
+	players := data.Players
+	playersData := make([]PlayerData, 0, len(players))
+
+	for i := range players {
+		p := players[i]
+		vec := p.Position()
+		pos := Position{X: vec.X, Y: vec.Y, Z: vec.Z}
+
+		playersData = append(playersData, PlayerData{Position: pos})
 	}
+
+	return StateResult{Players: playersData}
 }
