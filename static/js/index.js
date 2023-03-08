03c2c17717d7e59a5d88a3318d60a02d31039c7c
asadzeynal <asadzeynal9@gmail.com>
2023-03-08 16:24:52 +0400

    Draw infernos

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,14 @@ let ct_p;
 let t_p;
 let mapCanvas;
 let playersCanvas;
-let homeElement, screenElement
-const dimensions = 900
-let imgFlash = new Image(), imgHe = new Image(), imgDecoy = new Image(), imgSmoke = new Image(), imgIncendiary = new Image(), imgMolo = new Image();
+let homeElement, screenElement;
+const dimensions = 900;
+let imgFlash = new Image(),
+    imgHe = new Image(),
+    imgDecoy = new Image(),
+    imgSmoke = new Image(),
+    imgIncendiary = new Image(),
+    imgMolo = new Image();
 
 function loadNades() {
     imgFlash.src = "../img/nade_flash.webp";
@@ -17,11 +22,11 @@ function loadNades() {
 
 function init() {
     homeElement = document.getElementById("home");
-    screenElement = document.getElementById("screen")
+    screenElement = document.getElementById("screen");
     mapCanvas = document.getElementById("game_map");
     mapCanvas.width = dimensions;
     mapCanvas.height = dimensions;
-    document.getElementById("canvas_container").style.height = dimensions
+    document.getElementById("canvas_container").style.height = dimensions;
     document.getElementById("canvas_container").style.width = dimensions;
     playersCanvas = document.getElementById("players_canvas");
     playersCanvas.width = dimensions;
@@ -31,7 +36,7 @@ function init() {
 
 function drawMap(mapName) {
     homeElement.style.display = "none";
-    screenElement.classList.remove('hidden');
+    screenElement.classList.remove("hidden");
     ctx_map = mapCanvas.getContext("2d");
     ctx_map.scale(dimensions / 1024, dimensions / 1024);
     const img = new Image();
@@ -39,7 +44,7 @@ function drawMap(mapName) {
         ctx_map.drawImage(img, 0, 0);
     };
     img.src = `../img/${mapName}_radar.png`;
-};
+}
 
 function updateState(currentState) {
     ctx_pl = playersCanvas.getContext("2d");
@@ -52,11 +57,16 @@ function updateState(currentState) {
 
     document.getElementById("clan_tag_t").textContent = teamT.ClanTag;
     document.getElementById("clan_tag_ct").textContent = teamCt.ClanTag;
-    document.getElementById("score_t").textContent = teamT.Score
-    document.getElementById("score_ct").textContent = teamCt.Score
+    document.getElementById("score_t").textContent = teamT.Score;
+    document.getElementById("score_ct").textContent = teamCt.Score;
 
     ctx_pl.reset();
+    ctx_pl.globalCompositeOperation = "destination-over";
     ctx_pl.scale(dimensions / 1024, dimensions / 1024);
+
+    drawInfernos(ctx_pl, state.Infernos);
+    drawNades(ctx_pl, state.Nades);
+
     for (let i = 0; i < teamT.Players.length + teamCt.Players.length; i++) {
         p = [...teamT.Players, ...teamCt.Players].sort(sortPlayersById)[i];
         if (!p.IsAlive) {
@@ -67,9 +77,19 @@ function updateState(currentState) {
     }
 
     fillTable(teamT, teamCt);
+}
 
-    let nades = state.Nades;
-    drawNades(ctx_pl, nades);
+function drawInfernos(ctx, infernos) {
+    for (let i = 0; i < infernos.length; i++) {
+        inferno = infernos[i];
+        ctx.moveTo(inferno.BorderPositions[0].X, inferno.BorderPositions[0].Y);
+        ctx.fillStyle = "#ff704d";
+        for (let j = 1; j < inferno.BorderPositions.length; j++) {
+            bp = inferno.BorderPositions[j];
+            ctx.lineTo(bp.X, bp.Y);
+        }
+        ctx.fill();
+    }
 }
 
 function drawNades(ctx, nades) {
@@ -79,7 +99,7 @@ function drawNades(ctx, nades) {
         ctx.moveTo(nade.Positions[0].X, nade.Positions[0].Y);
         let lastPosX, lastPosY;
         for (let j = 1; j < nade.Positions.length; j++) {
-            pos = nade.Positions[j]
+            pos = nade.Positions[j];
             ctx.lineTo(pos.X, pos.Y);
             ctx.moveTo(pos.X, pos.Y);
             lastPosX = pos.X;
@@ -110,34 +130,34 @@ function getNadeImg(type) {
         return imgSmoke;
     }
 
-    return null
+    return null;
 }
 
 function sortPlayersByScore(a, b) {
-    let scoreA = (a.Kills * 2) + a.Assists;
-    let scoreB = (b.Kills * 2) + b.Assists;
+    let scoreA = a.Kills * 2 + a.Assists;
+    let scoreB = b.Kills * 2 + b.Assists;
     let diff = scoreB - scoreA;
     if (diff == 0) {
-        return a.Id - b.Id
+        return a.Id - b.Id;
     }
-    return diff
+    return diff;
 }
 
 function sortPlayersById(a, b) {
-    return a.Id - b.Id
+    return a.Id - b.Id;
 }
 
 function fillTable(teamT, teamCt) {
     teamT.Players.sort(sortPlayersByScore);
     for (let i = 0; i < teamT.Players.length; i++) {
-        let row = document.getElementById(`p${i + 1}`)
-        insertPlayerData(row, teamT.Players[i])
+        let row = document.getElementById(`p${i + 1}`);
+        insertPlayerData(row, teamT.Players[i]);
     }
 
     teamCt.Players.sort(sortPlayersByScore);
     for (let i = 0; i < teamCt.Players.length; i++) {
-        let row = document.getElementById(`p${i + 6}`)
-        insertPlayerData(row, teamCt.Players[i])
+        let row = document.getElementById(`p${i + 6}`);
+        insertPlayerData(row, teamCt.Players[i]);
     }
 }
 
@@ -146,69 +166,84 @@ function insertPlayerData(row, player) {
     row.querySelector("#hp").textContent = player.HP;
     row.querySelector("#money").textContent = player.Money;
     row.querySelector("#equipped").textContent = player.Equipped;
-    row.querySelector("#kda").textContent = `${player.Kills}/${player.Deaths}/${player.Assists}`;
+    row.querySelector(
+        "#kda"
+    ).textContent = `${player.Kills}/${player.Deaths}/${player.Assists}`;
 }
 
 function displayTimeLeft(duration) {
-    let timeElement = document.getElementById("time_left")
-    let minutes = Math.floor(duration / 60)
-    let seconds = duration % 60
+    let timeElement = document.getElementById("time_left");
+    let minutes = Math.floor(duration / 60);
+    let seconds = duration % 60;
     if (seconds < 10) {
-        seconds = `0${seconds}`
+        seconds = `0${seconds}`;
     }
-    timeElement.textContent = `${minutes}:${seconds}`
+    timeElement.textContent = `${minutes}:${seconds}`;
 }
 
 function drawDeadPlayer(ctx, p) {
-    pos = p.LastAlivePosition
+    pos = p.LastAlivePosition;
     if (p.Team == 2) {
-        ctx.strokeStyle = "orange"
+        ctx.strokeStyle = "orange";
     } else {
-        ctx.strokeStyle = "#219ebc"
+        ctx.strokeStyle = "#219ebc";
     }
     ctx.beginPath();
     ctx.moveTo(pos.X - 5, pos.Y - 5);
     ctx.lineTo(pos.X + 5, pos.Y + 5);
     ctx.moveTo(pos.X + 5, pos.Y - 5);
     ctx.lineTo(pos.X - 5, pos.Y + 5);
-    ctx.closePath()
-    ctx.stroke()
+    ctx.closePath();
+    ctx.stroke();
 }
 
 function drawAlivePlayer(ctx, p) {
-    const r = 7
-    pos = p.Position
+    const r = 7;
+    pos = p.Position;
     let fillStyle;
     if (p.Team == 2) {
-        fillStyle = "orange"
+        fillStyle = "orange";
     } else {
-        fillStyle = "#219ebc"
+        fillStyle = "#219ebc";
     }
 
-    var hpDown = (100 - p.HP) / 100
+    var hpDown = (100 - p.HP) / 100;
 
     ctx.save();
     ctx.fillStyle = fillStyle;
-    ctx.globalCompositeOperation = "destination-over";
     ctx.beginPath();
     ctx.translate(pos.X, pos.Y);
-    ctx.rotate((- p.ViewDirection - 90) * Math.PI / 180.0);
+    ctx.rotate(((-p.ViewDirection - 90) * Math.PI) / 180.0);
     ctx.translate(-pos.X, -pos.Y);
-    ctx.arc(pos.X, pos.Y, r, 0.5 * Math.PI + hpDown * Math.PI + 0.001, 0.5 * Math.PI - hpDown * Math.PI, false);
+    ctx.arc(
+        pos.X,
+        pos.Y,
+        r,
+        0.5 * Math.PI + hpDown * Math.PI + 0.001,
+        0.5 * Math.PI - hpDown * Math.PI,
+        false
+    );
     ctx.closePath();
     ctx.fill();
-    
+
     ctx.beginPath();
     ctx.fillStyle = "black";
-    ctx.arc(pos.X, pos.Y, r, 0.5 * Math.PI + hpDown * Math.PI + 0.001, 0.5 * Math.PI - hpDown * Math.PI, true);
-    
+    ctx.arc(
+        pos.X,
+        pos.Y,
+        r,
+        0.5 * Math.PI + hpDown * Math.PI + 0.001,
+        0.5 * Math.PI - hpDown * Math.PI,
+        true
+    );
+
     ctx.closePath();
     ctx.fill();
     ctx.fillStyle = fillStyle;
 
-    ctx.beginPath()
-    ctx.strokeStyle = "white"
-    ctx.lineWidth = 2
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 2;
     ctx.arc(pos.X, pos.Y, r + 0.5, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.closePath();
