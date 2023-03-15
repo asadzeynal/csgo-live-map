adb77a3116da8a2c43cb32219625b5c6d2c1ee62
asadzeynal <asadzeynal9@gmail.com>
2023-03-15 20:40:48 +0400

    added port env variable

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	log.Println("Listening on http://localhost:3000/index.html")
-	err := http.ListenAndServe(":3000", nil)
+	port := os.Getenv("DEMOVOI_PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Printf("Listening on http://localhost:%v/index.html\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
